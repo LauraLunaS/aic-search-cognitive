1c8892b2c88f26646b4caf3c2133a07af894ea92
Laura Luna <lauraluna.siqueira@hotmail.com>
2023-09-22 15:52:13 -0300

    first commit

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,9 @@
+import SearchName from '../../pages/submit'
+
+export default function HomePage() {
+    return (
+        <div>
+            <SearchName />
+        </div>
+    )
+}
---
+++ src/pages/submit/style.ts
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+
+export const Container = styled.div `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background-color: #F1F0F0;
+    width: 100%;
+    height: 100%;
+`;
+
+export const InputText = styled.input `
+    background-color: #d9d9d9;
+    background-image: url('/searchIcon.png');
+    background-repeat: no-repeat;
+    background-position: right 5px;
+    border: none;
+    width: 100%;
+    border-radius: 5px;
+    height: 2rem;
+    padding: 1rem;
+    margin: 1rem;
+
+    @media (min-width: 450px) {
+        width: 50%;
+    }
+
+    @media (min-width: 1025px) {
+        width: 40%;
+    }
+
+    @media (min-width: 1280px) {
+        width: 30%;
+        height: 2.3rem;
+        background-position: right 8px;
+    }
+`;
+
+export const Form = styled.form `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+`;
+
+export const ContainerResult = styled.div `
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    flex-direction: column;
+`;
+
+export const Result = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+    flex-direction: column;
+    font-family: 'Montserrat';
+`;
